6d2c82bc749ef604a957ff075d09fc69e7a12b9a
MikhailArkhipov <mikhaila@microsoft.com>
2018-09-07 13:00:28 -0700

    Fix stack overflow in tests

--- src/Analysis/Engine/Test/LanguageServerTests.cs
+++ src/Analysis/Engine/Test/LanguageServerTests.cs
@@ -1013,7 +1013,7 @@ datetime.datetime.now().day
 
                 public string Name => "getall";
 
-                public void Dispose() => _server.Dispose();
+                public void Dispose() { }
 
                 public Task<IReadOnlyDictionary<string, object>> ExecuteCommand(string command, IReadOnlyDictionary<string, object> properties, CancellationToken token) {
                     if (properties == null) {
