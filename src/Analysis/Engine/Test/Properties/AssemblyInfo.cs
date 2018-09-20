507419b0f40d1d3dde8b436d354cf34db9446635
Alexander Sher <alsher@microsoft.com>
2018-09-20 12:57:52 -0700

    Generate assembly versions

--- src/Analysis/Engine/Test/Properties/AssemblyInfo.cs
+++ src/Analysis/Engine/Test/Properties/AssemblyInfo.cs
@@ -9,16 +9,13 @@
 // THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
 // OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
 // IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-// MERCHANTABLITY OR NON-INFRINGEMENT.
+// MERCHANTABILITY OR NON-INFRINGEMENT.
 //
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("AnalysisTests")]
-[assembly: AssemblyDescription("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3e11e47d-2321-4769-96b9-38b1eaf8e4eb")]
