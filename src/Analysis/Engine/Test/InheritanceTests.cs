40c24df465333be79846c23e2bf50c6112363e7f
Alexander Sher <alsher@microsoft.com>
2018-09-20 13:32:38 -0700

    Port PR from PTVS: https://github.com/Microsoft/PTVS/pull/4716

---
+++ src/Analysis/Engine/Test/InheritanceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Python.LanguageServer.Implementation;
+using Microsoft.PythonTools.Analysis.FluentAssertions;
+using Microsoft.PythonTools.Interpreter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestUtilities;
+
+namespace Microsoft.PythonTools.Analysis {
+    [TestClass]
+    public class InheritanceTests {
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize() => TestEnvironmentImpl.TestInitialize($"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}");
+
+        [TestCleanup]
+        public void TestCleanup() => TestEnvironmentImpl.TestCleanup();
+
+        [TestMethod]
+        public async Task AbstractMethodReturnTypeIgnored() {
+            var code = @"import abc
+class A:
+    @abc.abstractmethod
+    def virt():
+        pass
+class B(A):
+    def virt():
+        return 42
+a = A()
+b = a.virt()";
+
+            using (var server = await new Server().InitializeAsync(PythonVersions.Required_Python36X)) {
+                var analysis = await server.OpenDefaultDocumentAndGetAnalysisAsync(code);
+
+                analysis.Should().HaveVariable("b").OfType(BuiltinTypeId.Int);
+            }
+        }
+    }
+}
