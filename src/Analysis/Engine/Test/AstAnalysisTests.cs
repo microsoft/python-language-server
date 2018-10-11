5ad1b0653ac2c906263d205ad76b52026d4dcb66
Mikhail Arkhipov <mikhaila@microsoft.com>
2018-10-10 21:04:39 -0700

    Re-enable tests

--- src/Analysis/Engine/Test/AstAnalysisTests.cs
+++ src/Analysis/Engine/Test/AstAnalysisTests.cs
@@ -196,7 +196,6 @@ namespace AnalysisTests {
         }
 
         [TestMethod, Priority(0)]
-        [Ignore("https://github.com/Microsoft/python-language-server/issues/234")]
         public async Task AstMultiValues() {
             using (var server = await CreateServerAsync()) {
                 var analysis = await server.OpenDefaultDocumentAndGetAnalysisAsync("from MultiValues import *");
@@ -241,7 +240,6 @@ R_A3 = R_A1.r_A()";
         }
 
         [TestMethod, Priority(0)]
-        [Ignore("https://github.com/Microsoft/python-language-server/issues/233")]
         public async Task AstInstanceMembers() {
             using (var server = await CreateServerAsync()) {
                 var analysis = await server.OpenDefaultDocumentAndGetAnalysisAsync("from InstanceMethod import f1, f2");
