148d0445a0ba726291992d477bc2e5a03df1e7b6
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2018-11-14 09:54:09 -0800

    Include assembly version number in telemetry event properties, standardize TelemetryEvent creation (#389)
    * Include assembly version number in telemetry event properties
    
    * move telemetry helpers to Telemetry.cs, add helper to create a more standardized TelemetryEvent with name prefix and version
    
    * remove dead version code from listener
    
    * add plsVersion to all outgoing telemetry events

--- src/LanguageServer/Impl/Telemetry.cs
+++ src/LanguageServer/Impl/Telemetry.cs
@@ -21,6 +21,14 @@ using Microsoft.PythonTools.Analysis.Infrastructure;
 using StreamJsonRpc;
 
 namespace Microsoft.Python.LanguageServer.Implementation {
+    internal class Telemetry {
+        private const string EventPrefix = "python_language_server/";
+
+        public static TelemetryEvent CreateEvent(string eventName) => new TelemetryEvent {
+            EventName = EventPrefix + eventName,
+        };
+    }
+
     internal class TelemetryRpcTraceListener : TraceListener {
         private readonly ITelemetryService2 _telemetryService;
 
@@ -64,9 +72,7 @@ namespace Microsoft.Python.LanguageServer.Implementation {
                 return;
             }
 
-            var e = new TelemetryEvent {
-                EventName = "python_language_server/rpc.exception", // TODO: Create EventName with a standardized format elsewhere.
-            };
+            var e = Telemetry.CreateEvent("rpc.exception");
             e.Properties["method"] = method;
             e.Properties["name"] = exception.GetType().Name;
             e.Properties["stackTrace"] = exception.StackTrace;
