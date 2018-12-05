780757add54916958e7612bfc65d7ff5a93a2d67
Mikhail Arkhipov <mikhaila@microsoft.com>
2018-12-05 10:21:42 -0800

    Fix #446

--- src/Analysis/Engine/Impl/Interpreter/Ast/AstTypeAnnotationConverter.cs
+++ src/Analysis/Engine/Impl/Interpreter/Ast/AstTypeAnnotationConverter.cs
@@ -198,7 +198,7 @@ namespace Microsoft.PythonTools.Interpreter.Ast {
         private IPythonType MakeGenericClassType(IPythonType typeArg) {
             if (typeArg.IsBuiltin) {
                 var type = _scope.Interpreter.GetBuiltinType(typeArg.TypeId) as AstPythonType;
-                return type.TypeId == BuiltinTypeId.Unknown
+                return type == null || type.TypeId == BuiltinTypeId.Unknown
                     ? _scope.Interpreter.GetBuiltinType(BuiltinTypeId.Type)
                     : type.GetTypeFactory();
             }
