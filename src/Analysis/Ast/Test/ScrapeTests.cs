36fec2bd2fcd4c7ee664de79d4471f57b876bafa
MikhailArkhipov <mikhaila@microsoft.com>
2018-12-18 11:42:52 -0800

    Built-in scrape tests

--- src/Analysis/Ast/Test/ScrapeTests.cs
+++ src/Analysis/Ast/Test/ScrapeTests.cs
@@ -17,6 +17,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Python.Analysis.Core.Interpreter;
@@ -74,7 +75,7 @@ namespace Microsoft.Python.Analysis.Tests {
 
             var services = await CreateServicesAsync(moduleDirectory, configuration);
             var interpreter = services.GetService<IPythonInterpreter>();
-            
+
             // TODO: this is Windows only
             var dllsDir = Path.Combine(Path.GetDirectoryName(interpreter.Configuration.LibraryPath), "DLLs");
             if (!Directory.Exists(dllsDir)) {
@@ -122,6 +123,70 @@ namespace Microsoft.Python.Analysis.Tests {
             }
 
             report.Should().BeEmpty();
+        }
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeV38() => await BuiltinScrape(PythonVersions.Python38_x64 ?? PythonVersions.Python38);
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeV37() => await BuiltinScrape(PythonVersions.Python37_x64 ?? PythonVersions.Python37);
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeV36() => await BuiltinScrape(PythonVersions.Python36_x64 ?? PythonVersions.Python36);
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeV35() => await BuiltinScrape(PythonVersions.Python35_x64 ?? PythonVersions.Python35);
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeV27() => await BuiltinScrape(PythonVersions.Python27_x64 ?? PythonVersions.Python27);
+
+        [TestMethod, Priority(0)]
+        public async Task BuiltinScrapeIPy27() => await BuiltinScrape(PythonVersions.IronPython27_x64 ?? PythonVersions.IronPython27);
+
+        private async Task BuiltinScrape(InterpreterConfiguration configuration) {
+            configuration.AssertInstalled();
+            var moduleUri = TestData.GetDefaultModuleUri();
+            var moduleDirectory = Path.GetDirectoryName(moduleUri.LocalPath);
+
+            var services = await CreateServicesAsync(moduleDirectory, configuration);
+            var interpreter = services.GetService<IPythonInterpreter>();
+
+            var mod = await interpreter.ModuleResolution.ImportModuleAsync(interpreter.ModuleResolution.BuiltinModuleName, new CancellationTokenSource(5000).Token);
+            Assert.IsInstanceOfType(mod, typeof(BuiltinsPythonModule));
+            var modPath = interpreter.ModuleResolution.ModuleCache.GetCacheFilePath(interpreter.Configuration.InterpreterPath);
+
+            var doc = mod as IDocument;
+            var errors = doc.GetDiagnostics();
+            foreach (var err in errors) {
+                Console.WriteLine(err);
+            }
+            Assert.AreEqual(0, errors.Count(), "Parse errors occurred");
+
+            var ast = await doc.GetAstAsync();
+            var seen = new HashSet<string>();
+            foreach (var stmt in ((SuiteStatement)ast.Body).Statements) {
+                if (stmt is ClassDefinition cd) {
+                    Assert.IsTrue(seen.Add(cd.Name), $"Repeated use of {cd.Name} at index {cd.StartIndex} in {modPath}");
+                } else if (stmt is FunctionDefinition fd) {
+                    Assert.IsTrue(seen.Add(fd.Name), $"Repeated use of {fd.Name} at index {fd.StartIndex} in {modPath}");
+                } else if (stmt is AssignmentStatement assign && assign.Left.FirstOrDefault() is NameExpression n) {
+                    Assert.IsTrue(seen.Add(n.Name), $"Repeated use of {n.Name} at index {n.StartIndex} in {modPath}");
+                }
+            }
+
+            // Ensure we can get all the builtin types
+            foreach (BuiltinTypeId v in Enum.GetValues(typeof(BuiltinTypeId))) {
+                var type = interpreter.GetBuiltinType(v);
+                type.Should().NotBeNull().And.BeAssignableTo<IPythonType>($"Did not find {v}");
+                type.IsBuiltin.Should().BeTrue();
+            }
+
+            // Ensure we cannot see or get builtin types directly
+            mod.GetMemberNames().Should().NotContain(Enum.GetNames(typeof(BuiltinTypeId)).Select(n => $"__{n}"));
+
+            foreach (var id in Enum.GetNames(typeof(BuiltinTypeId))) {
+                mod.GetMember($"__{id}").Should().BeNull(id);
+            }
         }
     }
 }
