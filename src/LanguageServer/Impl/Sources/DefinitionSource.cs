7eac7b334807b483c167ca53a32fc96028c7ea86
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-01-30 23:11:56 -0800

    Fixes

--- src/LanguageServer/Impl/Sources/DefinitionSource.cs
+++ src/LanguageServer/Impl/Sources/DefinitionSource.cs
@@ -13,14 +13,17 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
+using System.Net.WebSockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Python.Analysis;
+using Microsoft.Python.Analysis.Analyzer;
 using Microsoft.Python.Analysis.Analyzer.Expressions;
 using Microsoft.Python.Analysis.Documents;
 using Microsoft.Python.Analysis.Modules;
 using Microsoft.Python.Analysis.Types;
 using Microsoft.Python.Analysis.Values;
+using Microsoft.Python.Core;
 using Microsoft.Python.Core.Text;
 using Microsoft.Python.LanguageServer.Completion;
 using Microsoft.Python.LanguageServer.Protocol;
@@ -40,54 +43,71 @@ namespace Microsoft.Python.LanguageServer.Sources {
             }
 
             var eval = analysis.ExpressionEvaluator;
-            Node node = null;
-            IPythonModule module = null;
             using (eval.OpenScope(exprScope)) {
                 var value = await eval.GetValueFromExpressionAsync(expr, cancellationToken);
+                return await FromMemberAsync(value, expr, eval, cancellationToken);
+            }
+        }
 
-                switch (value) {
-                    case IPythonClassType cls:
-                        node = cls.ClassDefinition;
-                        module = cls.DeclaringModule;
-                        break;
-                    case IPythonFunctionType fn:
-                        node = fn.FunctionDefinition;
-                        module = fn.DeclaringModule;
-                        break;
-                    case IPythonPropertyType prop:
-                        node = prop.FunctionDefinition;
-                        module = prop.DeclaringModule;
-                        break;
-                    case IPythonModule mod: {
-                            var member = eval.LookupNameInScopes(mod.Name, out var scope);
-                            if (member != null && scope != null) {
-                                var v = scope.Variables[mod.Name];
-                                if (v != null) {
-                                    return new Reference { range = v.Location.Span, uri = v.Location.DocumentUri };
-                                }
+        private async Task<Reference> FromMemberAsync(IMember value, Expression expr, IExpressionEvaluator eval, CancellationToken cancellationToken) {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            Node node = null;
+            IPythonModule module = null;
+
+            switch (value) {
+                case IPythonClassType cls:
+                    node = cls.ClassDefinition;
+                    module = cls.DeclaringModule;
+                    break;
+                case IPythonFunctionType fn:
+                    node = fn.FunctionDefinition;
+                    module = fn.DeclaringModule;
+                    break;
+                case IPythonPropertyType prop:
+                    node = prop.FunctionDefinition;
+                    module = prop.DeclaringModule;
+                    break;
+                case IPythonModule mod: {
+                        var member = eval.LookupNameInScopes(mod.Name, out var scope);
+                        if (member != null && scope != null) {
+                            var v = scope.Variables[mod.Name];
+                            if (v != null) {
+                                return new Reference { range = v.Location.Span, uri = v.Location.DocumentUri };
                             }
-                            break;
                         }
-                    case IPythonInstance instance when instance.Type is IPythonFunctionType ft: {
-                            node = ft.FunctionDefinition;
-                            module = ft.DeclaringModule;
-                            break;
-                        }
-                    case IPythonInstance _ when expr is NameExpression nex: {
-                            var member = eval.LookupNameInScopes(nex.Name, out var scope);
-                            if (member != null && scope != null) {
-                                var v = scope.Variables[nex.Name];
-                                if (v != null) {
-                                    return new Reference { range = v.Location.Span, uri = v.Location.DocumentUri };
-                                }
+                        break;
+                    }
+                case IPythonInstance instance when instance.Type is IPythonFunctionType ft: {
+                        node = ft.FunctionDefinition;
+                        module = ft.DeclaringModule;
+                        break;
+                    }
+                case IPythonInstance _ when expr is NameExpression nex: {
+                        var member = eval.LookupNameInScopes(nex.Name, out var scope);
+                        if (member != null && scope != null) {
+                            var v = scope.Variables[nex.Name];
+                            if (v != null) {
+                                return new Reference { range = v.Location.Span, uri = v.Location.DocumentUri };
                             }
-                            break;
                         }
-                }
+                        break;
+                    }
+                case IPythonInstance _ when expr is MemberExpression mex: {
+                        var target = await eval.GetValueFromExpressionAsync(mex.Target, cancellationToken);
+                        var type = target?.GetPythonType();
+                        var member = type?.GetMember(mex.Name);
+                        if (member is IPythonInstance v) {
+                            return new Reference { range = v.Location.Span, uri = v.Location.DocumentUri };
+                        }
+                        return await FromMemberAsync(member, null, eval, cancellationToken);
+                    }
             }
 
             if (node != null && CanNavigateToModule(module) && module is IDocument doc) {
-                return new Reference { range = node.GetSpan(doc.GetAnyAst()), uri = doc.Uri };
+                return new Reference {
+                    range = node.GetSpan(doc.GetAnyAst()), uri = doc.Uri
+                };
             }
 
             return null;
