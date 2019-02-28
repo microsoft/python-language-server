d2cea25bd026be94abb8926cca93f41d8d5419a6
Hugues Valois <huvalo@microsoft.com>
2019-02-28 14:34:14 -0800

    Ensure that the EndIndex of a suite statement does not include the new line(s) at the end.

---
+++ src/UnitTests/TestData/Grammar/FunctionLocations.py
@@ -0,0 +1,35 @@
+def if_statement():
+    if True:
+        x = 1
+def if_else_statement():
+    if True:
+        x = 1
+    else:
+        x = 1
+def with_statement():
+    with None:
+        x = 1
+def try_statement():
+    try:
+        x = 1
+def try_except_statement():
+    try:
+        x = 1
+    except:
+        x = 1
+def try_else_statement():
+    try:
+        x = 1
+    except:
+        x = 1
+    else:
+        x = 1
+def for_statement():
+    for i in range(0, 10):
+        x = 1
+def assign_statements():
+    x = 1
+    x = 2
+    x = 3
+def assign_statement():
+    x = 1
