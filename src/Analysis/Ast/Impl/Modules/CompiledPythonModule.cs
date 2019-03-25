4bdd74886fdde8c622db47375b5014d3e567ff21
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2019-03-25 15:59:07 -0700

    Silence scraper stderr (#823)
    Fixes #817.
    
    This was more complicated than just asking to redirect stderr, as it needs to be read asynchronously to prevent a deadlock while reading both stderr and stdout.

--- src/Analysis/Ast/Impl/Modules/CompiledPythonModule.cs
+++ src/Analysis/Ast/Impl/Modules/CompiledPythonModule.cs
@@ -85,7 +85,8 @@ namespace Microsoft.Python.Analysis.Modules {
                 CreateNoWindow = true,
                 StandardOutputEncoding = Encoding.UTF8,
                 RedirectStandardInput = true,
-                RedirectStandardOutput = true
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
             };
             var ps = Services.GetService<IProcessServices>();
 
