e86edf11b2ede90696ab5ff1713114e546ca67d7
Brian Bokser <brianbok@users.noreply.github.com>
2019-04-05 15:04:03 -0700

    Remove check for indexes in parser tests (#885)
    Tests that check errors reported by parser were expecting specific index numbers.
    Index numbers differ by OS (based on usage of CRLF/LF, etc) so I removed the check fixing the tests for other platforms. Notice that index numbers remain there unused.
    This prepares us for a different commit which would delete ErrorInfo and all those numbers from our tests.
    I also added tokenizer tests for different newlines.

--- src/Parsing/Test/TokenizerTest.cs
+++ src/Parsing/Test/TokenizerTest.cs
@@ -37,11 +37,73 @@ namespace Microsoft.Python.Parsing.Tests {
             }
         }
 
+        [TestMethod, Priority(0)]
+        public void CRLFNewLines() {
+            foreach (var version in AllVersions) {
+                var code = "\r\nx\r\ny\r\n";
+
+                var initialLocation = SourceLocation.MinValue;
+                var tokenizer = MakeTokenizer(version, TokenizerOptions.None, code,
+                    initialLocation);
+
+                CheckAndReadNext(tokenizer, new IndexSpan(0, 2), TokenKind.NLToken);
+                CheckAndReadNext(tokenizer, new IndexSpan(2, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(3, 2), TokenKind.NewLine);
+                CheckAndReadNext(tokenizer, new IndexSpan(5, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(6, 2), TokenKind.NewLine);
+            }
+        }
+
+        [TestMethod, Priority(0)]
+        public void CombineNewLines() {
+            foreach (var version in AllVersions) {
+                var code = "\r\nx\ny\r\n";
+
+                var initialLocation = SourceLocation.MinValue;
+                var tokenizer = MakeTokenizer(version, TokenizerOptions.None, code,
+                    initialLocation);
+
+                CheckAndReadNext(tokenizer, new IndexSpan(0, 2), TokenKind.NLToken);
+                CheckAndReadNext(tokenizer, new IndexSpan(2, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(3, 1), TokenKind.NewLine);
+                CheckAndReadNext(tokenizer, new IndexSpan(4, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(5, 2), TokenKind.NewLine);
+            }
+        }
+
+        [TestMethod, Priority(0)]
+        public void LFNewLines() {
+            foreach (var version in AllVersions) {
+                var code = "\nx\ny\n";
+
+                var initialLocation = SourceLocation.MinValue;
+                var tokenizer = MakeTokenizer(version, TokenizerOptions.None, code,
+                    initialLocation);
+
+                CheckAndReadNext(tokenizer, new IndexSpan(0, 1), TokenKind.NLToken);
+                CheckAndReadNext(tokenizer, new IndexSpan(1, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(2, 1), TokenKind.NewLine);
+                CheckAndReadNext(tokenizer, new IndexSpan(3, 1), TokenKind.Name);
+                CheckAndReadNext(tokenizer, new IndexSpan(4, 1), TokenKind.NewLine);
+            }
+        }
+
+        private static void CheckAndReadNext(Tokenizer tokenizer, IndexSpan tokenSpan, TokenKind tokenKind) {
+            var token = tokenizer.GetNextToken();
+            tokenizer.TokenSpan.Should().Be(tokenSpan);
+            token.Kind.Should().Be(tokenKind);
+        }
+
         private Tokenizer MakeTokenizer(PythonLanguageVersion version, TokenizerOptions optionSet, string text,
+            SourceLocation? initialSourceLocation = null) {
+            return MakeTokenizer(version, optionSet, new StringReader(text), initialSourceLocation);
+        }
+
+        private Tokenizer MakeTokenizer(PythonLanguageVersion version, TokenizerOptions optionSet, StringReader reader,
             SourceLocation? initialSourceLocation = null) {
             var tokenizer = new Tokenizer(version, options: optionSet);
 
-            tokenizer.Initialize(null, new StringReader(text), initialSourceLocation ?? SourceLocation.MinValue);
+            tokenizer.Initialize(null, reader, initialSourceLocation ?? SourceLocation.MinValue);
             return tokenizer;
         }
     }
