809632f70428ecf5f239f967757e50cbe5b66692
MikhailArkhipov <mikhaila@microsoft.com>
2019-04-05 15:45:55 -0700

    Merge issues

--- src/Analysis/Ast/Impl/Analyzer/PythonAnalyzer.cs
+++ src/Analysis/Ast/Impl/Analyzer/PythonAnalyzer.cs
@@ -16,6 +16,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Python.Analysis.Dependencies;
