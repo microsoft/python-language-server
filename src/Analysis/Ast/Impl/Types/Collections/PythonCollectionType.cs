81600645110eb2ad565e8fe8e90c97aa67c0311b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2019-05-06 11:43:29 -0700

    remove leftover using

--- src/Analysis/Ast/Impl/Types/Collections/PythonCollectionType.cs
+++ src/Analysis/Ast/Impl/Types/Collections/PythonCollectionType.cs
@@ -14,7 +14,6 @@
 // permissions and limitations under the License.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Python.Analysis.Values;
