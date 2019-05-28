acad202d5049cdb1e700740d482821892ac4f6b5
MikhailArkhipov <mikhaila@microsoft.com>
2019-05-28 12:27:13 -0700

    Add options to keep data in memory

--- src/Analysis/Ast/Impl/Analyzer/Symbols/FunctionEvaluator.cs
+++ src/Analysis/Ast/Impl/Analyzer/Symbols/FunctionEvaluator.cs
@@ -80,7 +80,8 @@ namespace Microsoft.Python.Analysis.Analyzer.Symbols {
                     // Return type from the annotation is sufficient for libraries and stubs, no need to walk the body.
                     FunctionDefinition.Body?.Walk(this);
                     // For libraries remove declared local function variables to free up some memory.
-                    if (Module.ModuleType != ModuleType.User) {
+                    var optionsProvider = Eval.Services.GetService<IAnalysisOptionsProvider>();
+                    if (Module.ModuleType != ModuleType.User && optionsProvider?.Options.KeepLibraryLocalVariables != true) {
                         ((VariableCollection)Eval.CurrentScope.Variables).Clear();
                     }
                 }
