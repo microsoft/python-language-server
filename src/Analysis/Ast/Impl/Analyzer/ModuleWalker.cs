130b95dd48b18c534505e39e7cb1e10baf9c93e5
MikhailArkhipov <mikhaila@microsoft.com>
2019-05-29 16:32:22 -0700

    Fix test

--- src/Analysis/Ast/Impl/Analyzer/ModuleWalker.cs
+++ src/Analysis/Ast/Impl/Analyzer/ModuleWalker.cs
@@ -249,11 +249,6 @@ namespace Microsoft.Python.Analysis.Analyzer {
                     foreach (var name in stubType.GetMemberNames()) {
                         var stubMember = stubType.GetMember(name);
                         var member = cls.GetMember(name);
-                        // Do not pick members from base classes since otherwise we may end up
-                        // reassigning members of a different module or even of built-in types.
-                        if (member is IPythonClassMember clsm && !cls.Name.EqualsOrdinal(clsm.DeclaringType?.Name)) {
-                            continue;
-                        }
                         // Get documentation from the current type, if any, since stubs
                         // typically do not contain documentation while scraped code does.
                         member?.GetPythonType()?.TransferDocumentationAndLocation(stubMember.GetPythonType());
