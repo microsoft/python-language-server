3342e144c2431853547b5cf11acab8d881a2aa25
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-06-05 19:22:12 -0700

    Merge mess

--- src/Analysis/Ast/Impl/Analyzer/Symbols/FunctionEvaluator.cs
+++ src/Analysis/Ast/Impl/Analyzer/Symbols/FunctionEvaluator.cs
@@ -158,7 +158,6 @@ namespace Microsoft.Python.Analysis.Analyzer.Symbols {
             // Declare parameters in scope
             IMember defaultValue = null;
             for (var i = skip; i < FunctionDefinition.Parameters.Length; i++) {
-                var isGeneric = false;
                 var p = FunctionDefinition.Parameters[i];
                 if (!string.IsNullOrEmpty(p.Name)) {
                     var paramType = Eval.GetTypeFromAnnotation(p.Annotation, out var isGeneric);
