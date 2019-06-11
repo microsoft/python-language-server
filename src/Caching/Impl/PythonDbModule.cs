f23a4872ebd9d42cd31f22aa4e00fb71048aaba5
MikhailArkhipov <mikhaila@microsoft.com>
2019-06-11 15:04:12 -0700

    Partial

--- src/Caching/Impl/PythonDbModule.cs
+++ src/Caching/Impl/PythonDbModule.cs
@@ -16,6 +16,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using Microsoft.Python.Analysis.Caching.Models;
 using Microsoft.Python.Analysis.Modules;
 using Microsoft.Python.Analysis.Types;
@@ -27,69 +28,103 @@ namespace Microsoft.Python.Analysis.Caching {
     internal sealed class PythonDbModule : SpecializedModule {
         private readonly ModuleModel _model;
         private readonly IMember _unknownType;
+        private readonly GlobalScopeImpl _globalScope;
 
         public PythonDbModule(ModuleModel model, IServiceContainer services) 
             : base(model.Name, string.Empty, services) {
             _model = model;
             _unknownType = services.GetService<IPythonInterpreter>().UnknownType;
+            _globalScope = new GlobalScopeImpl(model, this);
         }
 
         protected override string LoadContent() => string.Empty;
 
-        public override IEnumerable<string> GetMemberNames() {
-            var classes = _model.Classes.Select(c => c.Name);
-            var functions = _model.Functions.Select(c => c.Name);
-            var variables = _model.Variables.Select(c => c.Name);
-            return classes.Concat(functions).Concat(variables);
-        }
-
-        public override IMember GetMember(string name) {
-            var v = _model.Variables.FirstOrDefault(c => c.Name == name);
-            if(v != null) {
-                return new Variable(name, Construct(v.Value), VariableSource.Declaration, new Location(this));
-            }
-            v = _model.Variables.FirstOrDefault(c => c.Name == name);
-            if (v != null) {
-                return new Variable(name, Construct(v.Value), VariableSource.Declaration, new Location(this));
-            }
-
-            return _unknownType;
-        }
-
-        public override IGlobalScope GlobalScope => base.GlobalScope;
-
-        private IMember Construct(string qualifiedName) {
-            var components = Split(qualifiedName, out var moduleName, out var isInstance);
-            return null;
-        }
-
-        private string[] Split(string qualifiedName, out string moduleName, out bool isInstance) {
-            isInstance = qualifiedName.StartsWith("i:");
-            qualifiedName = isInstance ? qualifiedName.Substring(2) : qualifiedName;
-            var components = qualifiedName.Split('.');
-            moduleName = components.Length > 0 ? components[0] : null;
-            return components.Length > 0 ? components.Skip(1).ToArray() : Array.Empty<string>();
-        }
+        public override string Documentation => _model.Documentation;
+        public override IEnumerable<string> GetMemberNames() => _globalScope.Variables.Names;
+        public override IMember GetMember(string name) => _globalScope.Variables[name];
+        public override IGlobalScope GlobalScope => _globalScope;
 
         private sealed class GlobalScopeImpl : IGlobalScope {
-            private readonly ModuleModel _model;
             private readonly VariableCollection _variables = new VariableCollection();
 
             public GlobalScopeImpl(ModuleModel model, IPythonModule module) {
-                _model = model;
                 Module = module;
 
-                //foreach (var v in model.Variables) {
-                //    _variables.DeclareVariable(v.Name, Construct(v.Value), VariableSource.Declaration, new Location(module));
-                //}
-                //foreach (var c in model.Classes) {
-                //    _variables.DeclareVariable(c.Name, Construct(c), VariableSource.Declaration, new Location(module));
-                //}
-                //foreach (var f in model.Functions) {
-                //    _variables.DeclareVariable(f.Name, Construct(f), VariableSource.Declaration, new Location(module));
-                //}
-                // TODO: classes and functions
-                // TODO: default variables
+                foreach (var c in model.Classes) {
+                    _variables.DeclareVariable(c.Name, ConstructClass(c), VariableSource.Declaration, new Location(module));
+                }
+                foreach (var f in model.Functions) {
+                    _variables.DeclareVariable(f.Name, ConstructFunction(f), VariableSource.Declaration, new Location(module));
+                }
+                foreach (var v in model.Variables) {
+                    var m = ConstructMember(v.Value);
+                    if (m != null) {
+                        _variables.DeclareVariable(v.Name, ConstructMember(v.Value), VariableSource.Declaration, new Location(module));
+                    }
+                }
+            }
+
+            private IPythonClassType ConstructClass(ClassModel cm) {
+                var cls = new PythonClassType(cm.Name, new Location(Module));
+                foreach (var f in cm.Methods) {
+                    cls.AddMember(f.Name, ConstructFunction(f), false);
+                }
+                foreach (var p in cm.Properties) {
+                    cls.AddMember(p.Name, ConstructProperty(p), false);
+                }
+                foreach (var c in cm.InnerClasses) {
+                    cls.AddMember(c.Name, ConstructClass(c), false);
+                }
+            }
+
+            private IPythonFunctionType ConstructFunction(FunctionModel fm, IPythonClassType cls) {
+                var ft = new PythonFunctionType(fm.Name, new Location(Module), cls, fm.Documentation);
+                foreach(var om in fm.Overloads) {
+                    var o = new PythonFunctionOverload(fm.Name, new Location(Module));
+                    o.SetDocumentation(fm.Documentation); // TODO: own documentation?
+                    o.SetReturnValue(ConstructMember(om.ReturnType), true);
+                    o.SetParameters(om.Parameters.Select(p => ConstructParameter(p)))
+                    ft.AddOverload(o);
+                }
+            }
+
+            private IPythonPropertyType ConstructProperty(PropertyModel pm, IPythonClassType cls) 
+                => new PythonPropertyType(pm.Name, new Location(Module), cls, (pm.Attributes & FunctionAttributes.Abstract) != 0);
+
+            private IParameterInfo ConstructParameter(ParameterModel pm)
+                => new ParameterInfo(pm.Name, ConstructType(pm.Type), pm.Kind, ConstructMember(pm.DefaultValue));
+
+            private IPythonType ConstructType(string qualifiedName) { }
+
+            private IMember ConstructMember(string qualifiedName) {
+                if (!SplitQualifiedName(qualifiedName, out var moduleName, out var typeName, out var isInstance)) {
+                    return null;
+                }
+
+                if(moduleName == Module.Name) {
+                    if(_model.g)
+                }
+                return isInstance ? new PythonInstance() : ;
+            }
+
+            private bool SplitQualifiedName(string qualifiedName, out string moduleName, out string typeName, out bool isInstance) {
+                moduleName = null;
+                typeName = null;
+
+                isInstance = qualifiedName.StartsWith("i:");
+                qualifiedName = isInstance ? qualifiedName.Substring(2) : qualifiedName;
+                var components = qualifiedName.Split('.');
+                switch (components.Length) {
+                    case 1:
+                        moduleName = "builtins";
+                        typeName = components[0];
+                        return true;
+                    case 2:
+                        moduleName = components[0];
+                        typeName = components[1];
+                        return true;
+                }
+                return false;
             }
 
             public string Name => _model.Name;
