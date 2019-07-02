3a63bb813c479115e5f5782711b5784e21b91976
Cameron Trando <CTrando@users.noreply.github.com>
2019-07-02 10:23:56 -0700

    Adding span check in test

--- src/Analysis/Ast/Test/LintGenericTests.cs
+++ src/Analysis/Ast/Test/LintGenericTests.cs
@@ -1,21 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
+using Microsoft.Python.Analysis.Tests.FluentAssertions;
+using Microsoft.Python.Parsing.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NSubstitute;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
 using TestUtilities;
+
 
 namespace Microsoft.Python.Analysis.Tests {
 
     [TestClass]
     public class LintGenericTests : AnalysisTestBase {
 
-        public const string GenericSetup = @"from typing import Generic, TypeVar
+        public const string GenericSetup = @"
+from typing import Generic, TypeVar
 T = TypeVar('T', int, str)
 T1 = TypeVar('T1', int, str)
+
+_X = TypeVar('_X', str, int)
+_T = _X
 ";
 
         public TestContext TestContext { get; set; }
@@ -27,107 +31,55 @@ T1 = TypeVar('T1', int, str)
         [TestCleanup]
         public void Cleanup() => TestEnvironmentImpl.TestCleanup();
 
-        [DataRow(GenericSetup + @"
-class Map(Generic[T, str]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[T, T1, int]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[T, str, int, T1]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[str, int]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[str]):
-    def hello():
-        pass
-")]
-
-
+        [DataRow("x = Generic[T, str]")]
+        [DataRow("x = Generic[T, T1, int]")]
+        [DataRow("x = Generic[T, str, int, T1]")]
+        [DataRow("x = Generic[str, int]")]
+        [DataRow("x = Generic[str]")]
         [DataTestMethod, Priority(0)]
-        public async Task GenericNotAllTypeParameters(string code) {
+        public async Task GenericNotAllTypeParameters(string decl) {
+            string code = GenericSetup + decl;
+
             var analysis = await GetAnalysisAsync(code);
             analysis.Diagnostics.Should().HaveCount(1);
 
             var diagnostic = analysis.Diagnostics.ElementAt(0);
+            var start = decl.IndexOf("Generic") + 1;
+            // adding 1 because SourceSpan.End is exclusive and another 1 because SourceSpan is 1-indexed
+            var end = decl.IndexOf("]", start) + 2;
+
+            diagnostic.SourceSpan.Should().Be(8, start, 8, end);
             diagnostic.ErrorCode.Should().Be(Diagnostics.ErrorCodes.TypingGenericArguments);
             diagnostic.Message.Should().Be(Resources.GenericNotAllTypeParameters);
         }
 
-        [DataRow(GenericSetup + @"
-_X = TypeVar('_X', str, int)
-_T = _X
-
-class Map(Generic[_T, _X]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-_X = TypeVar('_X', str, int)
-_T = _X
-
-class Map(Generic[_T, T, T1, _X]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-_X = TypeVar('_X', str, int)
-_T = _X
-
-class Map(Generic[_T,_T, T]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[T,T]):
-    def hello():
-        pass
-")]
+        [DataRow("x = Generic[_T, _X]")]
+        [DataRow("x = Generic[_T, T, T1, _X]")]
+        [DataRow("x = Generic[_T,_T, T]")]
+        [DataRow("x = Generic[T,T]")]
         [DataTestMethod, Priority(0)]
-        public async Task GenericDuplicateArguments(string code) {
+        public async Task GenericDuplicateArguments(string decl) {
+            string code = GenericSetup + decl;
             var analysis = await GetAnalysisAsync(code);
             analysis.Diagnostics.Should().HaveCount(1);
 
             var diagnostic = analysis.Diagnostics.ElementAt(0);
+            var start = decl.IndexOf("Generic") + 1;
+            // adding 1 because SourceSpan.End is exclusive and another 1 because SourceSpan is 1-indexed
+            var end = decl.IndexOf("]", start) + 2;
+            diagnostic.SourceSpan.Should().Be(8, start, 8, end);
+
             diagnostic.ErrorCode.Should().Be(Diagnostics.ErrorCodes.TypingGenericArguments);
             diagnostic.Message.Should().Be(Resources.GenericNotAllUnique);
         }
 
-        [DataRow(GenericSetup + @"
-_X = TypeVar('_X', str, int)
-
-class Map(Generic[_X, T]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[T1, T]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-class Map(Generic[T]):
-    def hello():
-        pass
-")]
-        [DataRow(GenericSetup + @"
-_X = TypeVar('_X', str, int)
-class Map(Generic[T,T1, _X]):
-    def hello():
-        pass
-")]
+        [DataRow("x = Generic[_X, T]")]
+        [DataRow("x = Generic[T1, T]")]
+        [DataRow("x = Generic[T]")]
+        [DataRow("x = Generic[T,T1, _X]")]
         [DataTestMethod, Priority(0)]
-        public async Task GenericArgumentsNoDiagnosticOnValid(string code) {
+        public async Task GenericArgumentsNoDiagnosticOnValid(string decl) {
+            string code = GenericSetup + decl;
             var analysis = await GetAnalysisAsync(code);
             analysis.Diagnostics.Should().HaveCount(0);
         }
