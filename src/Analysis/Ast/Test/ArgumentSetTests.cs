35673c8ca2164643581106cc351670b2e4f6d5d6
Cameron Trando <CTrando@users.noreply.github.com>
2019-07-11 14:15:41 -0700

    Adding tests for default params

--- src/Analysis/Ast/Test/ArgumentSetTests.cs
+++ src/Analysis/Ast/Test/ArgumentSetTests.cs
@@ -175,12 +175,15 @@ TypeVar('T', int, float, str, bound='test', covariant=True)
 ";
             var argSet = await GetArgSetAsync(code, funcName: "TypeVar");
             argSet.Arguments.Count.Should().Be(4);
+
             argSet.Arguments[0].Name.Should().Be("name");
             argSet.Arguments[0].ValueExpression.Should().BeOfType<ConstantExpression>().Which.Value.Should().Be("T");
             argSet.Arguments[1].Name.Should().Be("bound");
             argSet.Arguments[1].ValueExpression.Should().BeOfType<ConstantExpression>().Which.Value.Should().Be("test");
             argSet.Arguments[2].Name.Should().Be("covariant");
             argSet.Arguments[2].ValueExpression.Should().BeOfType<ConstantExpression>().Which.Value.Should().Be(true);
+            argSet.Arguments[3].Name.Should().Be("contravariant");
+            argSet.Arguments[3].Value.Should().BeOfType<PythonConstant>().Which.Value.Should().Be(false);
 
             argSet.ListArgument.Should().NotBeNull();
             argSet.ListArgument.Name.Should().Be("constraints");
@@ -188,6 +191,33 @@ TypeVar('T', int, float, str, bound='test', covariant=True)
 
             argSet.DictionaryArgument.Should().BeNull();
         }
+
+        [TestMethod, Priority(0)]
+        public async Task TypeVarDefaultValues() {
+            const string code = @"
+from typing import TypeVar
+
+TypeVar('T', int, float, str)
+";
+            var argSet = await GetArgSetAsync(code, funcName: "TypeVar");
+            argSet.Arguments.Count.Should().Be(4);
+
+            argSet.Arguments[0].Name.Should().Be("name");
+            argSet.Arguments[0].ValueExpression.Should().BeOfType<ConstantExpression>().Which.Value.Should().Be("T");
+            argSet.Arguments[1].Name.Should().Be("bound");
+            argSet.Arguments[1].Value.Should().BeOfType<PythonConstant>().Which.Value.Should().Be(null);
+            argSet.Arguments[2].Name.Should().Be("covariant");
+            argSet.Arguments[2].Value.Should().BeOfType<PythonConstant>().Which.Value.Should().Be(false);
+            argSet.Arguments[3].Name.Should().Be("contravariant");
+            argSet.Arguments[3].Value.Should().BeOfType<PythonConstant>().Which.Value.Should().Be(false);
+
+            argSet.ListArgument.Should().NotBeNull();
+            argSet.ListArgument.Name.Should().Be("constraints");
+            argSet.ListArgument.Expressions.OfType<NameExpression>().Select(c => c.Name).Should().ContainInOrder("int", "float", "str");
+
+            argSet.DictionaryArgument.Should().BeNull();
+        }
+
 
         [TestMethod, Priority(0)]
         public async Task NewTypeSpecializedArgs() {
