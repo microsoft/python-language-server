d79b06f2fc3b7e5ee5fcca3719ed726bb0634744
Cameron Trando <CTrando@users.noreply.github.com>
2019-07-15 16:01:13 -0700

    Comment on ignore test

--- src/Analysis/Ast/Test/LintInheritNonClassTests.cs
+++ src/Analysis/Ast/Test/LintInheritNonClassTests.cs
@@ -13,10 +13,13 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Python.Analysis.Analyzer;
 using Microsoft.Python.Analysis.Diagnostics;
+using Microsoft.Python.Analysis.Documents;
 using Microsoft.Python.Analysis.Tests.FluentAssertions;
 using Microsoft.Python.Core;
 using Microsoft.Python.Parsing.Tests;
@@ -143,8 +146,15 @@ class C(x + 2):
         }
 
 
+        /// <summary>
+        /// Because typing module is specialized with functions instead of classes,
+        /// we think that we are extending a function instead of a class so we would erroneously
+        /// give a diagnostic message
+        /// </summary>
+        /// <returns></returns>
+        [Ignore]
         [TestMethod, Priority(0)]
-        public async Task InheritFromOtherModule() {
+        public async Task InheritFromTypingModule() {
             const string code = @"
 import typing
 
@@ -157,21 +167,115 @@ class C(typing.TypeVar):
         }
 
         [TestMethod, Priority(0)]
-        public async Task InheritFromRenamedOtherModule() {
-            const string code = @"
-import typing
-
-tmp = typing.TypeVar
-class C(tmp):
-    def method(self):
-        return 'test'
+        public async Task InheritFromOtherModule() {
+            var module1Code = @"
+class B:
+    def hello(self):
+        pass
 ";
-            var analysis = await GetAnalysisAsync(code);
+
+            var appCode = @"
+from module1 import B
+
+class C(B):
+    def hello():
+        pass
+";
+
+            var module1Uri = TestData.GetTestSpecificUri("module1.py");
+            var appUri = TestData.GetTestSpecificUri("app.py");
+
+            var root = Path.GetDirectoryName(appUri.AbsolutePath);
+            await CreateServicesAsync(root, PythonVersions.LatestAvailable3X);
+            var rdt = Services.GetService<IRunningDocumentTable>();
+            var analyzer = Services.GetService<IPythonAnalyzer>();
+
+            rdt.OpenDocument(module1Uri, module1Code);
+
+            var app = rdt.OpenDocument(appUri, appCode);
+            await analyzer.WaitForCompleteAnalysisAsync();
+            var analysis = await app.GetAnalysisAsync();
             analysis.Diagnostics.Should().BeEmpty();
         }
 
         [TestMethod, Priority(0)]
-        public async Task InheritFromProperty() {
+        public async Task InheritFromPropertyReturnsFunction() {
+            const string code = @"
+def func(self):
+    pass
+
+class B:
+    @property
+    def test(self):
+        return func
+
+b = B()
+
+class C(b.test):
+    def method(self):
+        return 'test'
+";
+            var analysis = await GetAnalysisAsync(code);
+            analysis.Diagnostics.Should().HaveCount(1);
+
+            var diagnostic = analysis.Diagnostics.ElementAt(0);
+            diagnostic.SourceSpan.Should().Be(12, 7, 12, 8);
+            diagnostic.Message.Should().Be(Resources.InheritNonClass.FormatInvariant("b.test"));
+            diagnostic.ErrorCode.Should().Be(ErrorCodes.InheritNonClass);
+        }
+
+
+        [TestMethod, Priority(0)]
+        public async Task InheritFromPropertyReturnsMethod() {
+            const string code = @"
+class B:
+    @property
+    def test(self):
+        return self.func
+
+    def func(self):
+        pass
+
+b = B()
+
+class C(b.test):
+    def method(self):
+        return 'test'
+";
+            var analysis = await GetAnalysisAsync(code);
+            analysis.Diagnostics.Should().HaveCount(1);
+
+            var diagnostic = analysis.Diagnostics.ElementAt(0);
+            diagnostic.SourceSpan.Should().Be(12, 7, 12, 8);
+            diagnostic.Message.Should().Be(Resources.InheritNonClass.FormatInvariant("b.test"));
+            diagnostic.ErrorCode.Should().Be(ErrorCodes.InheritNonClass);
+        }
+
+        [TestMethod, Priority(0)]
+        public async Task InheritFromPropertyReturnsConstant() {
+            const string code = @"
+class B:
+    @property
+    def test(self):
+        return 5
+
+b = B()
+
+class C(b.test):
+    def method(self):
+        return 'test'
+";
+            var analysis = await GetAnalysisAsync(code);
+            analysis.Diagnostics.Should().HaveCount(1);
+
+            var diagnostic = analysis.Diagnostics.ElementAt(0);
+            diagnostic.SourceSpan.Should().Be(9, 7, 9, 8);
+            diagnostic.Message.Should().Be(Resources.InheritNonClass.FormatInvariant("b.test"));
+            diagnostic.ErrorCode.Should().Be(ErrorCodes.InheritNonClass);
+        }
+
+        [TestMethod, Priority(0)]
+        public async Task InheritFromEmptyPropertyNoDiagnostic() {
             const string code = @"
 class B:
     @property
@@ -185,12 +289,7 @@ class C(b.test):
         return 'test'
 ";
             var analysis = await GetAnalysisAsync(code);
-            analysis.Diagnostics.Should().HaveCount(1);
-
-            var diagnostic = analysis.Diagnostics.ElementAt(0);
-            diagnostic.SourceSpan.Should().Be(4, 7, 4, 8);
-            diagnostic.Message.Should().Be(Resources.InheritNonClass.FormatInvariant("b.test"));
-            diagnostic.ErrorCode.Should().Be(ErrorCodes.InheritNonClass);
+            analysis.Diagnostics.Should().BeEmpty();
         }
 
         [TestMethod, Priority(0)]
