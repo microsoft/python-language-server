73302796e673a008a59e164afef451a73b23e719
Cameron Trando <CTrando@users.noreply.github.com>
2019-07-16 15:21:05 -0700

    resolving imports

--- src/Analysis/Ast/Impl/Specializations/Typing/Types/GenericTypeParameter.cs
+++ src/Analysis/Ast/Impl/Specializations/Typing/Types/GenericTypeParameter.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Python.Analysis.Diagnostics;
 using Microsoft.Python.Analysis.Types;
 using Microsoft.Python.Analysis.Utilities;
