c88754bdb7f3463353f5a9067fa341116fc72f4d
Cameron <CTrando@users.noreply.github.com>
2019-07-17 18:22:54 -0700

    Give diagnostic when inheriting from something that is not a class (#1337)
    * When creating a class, give diagnostic if bases are not class types

--- src/Analysis/Ast/Impl/Diagnostics/ErrorCodes.cs
+++ src/Analysis/Ast/Impl/Diagnostics/ErrorCodes.cs
@@ -30,6 +30,7 @@ namespace Microsoft.Python.Analysis.Diagnostics {
         public const string ReturnInInit = "return-in-init";
         public const string TypingNewTypeArguments = "typing-newtype-arguments";
         public const string TypingGenericArguments = "typing-generic-arguments";
+        public const string InheritNonClass = "inherit-non-class";
         public const string DuplicateArgumentName = "duplicate-argument-name";
     }
 }
