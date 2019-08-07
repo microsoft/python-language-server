1d86086728b80dca59e23666448ddd7223df8e27
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-08-06 20:20:37 -0700

    Revert "Fix tests"
    This reverts commit 247a8c38741324452cc5a8eeb412a9784441bd8b.

--- src/Analysis/Ast/Impl/Analyzer/Handlers/FromImportHandler.cs
+++ src/Analysis/Ast/Impl/Analyzer/Handlers/FromImportHandler.cs
@@ -40,16 +40,8 @@ namespace Microsoft.Python.Analysis.Analyzer.Handlers {
                 }
             }
 
-            if(node.Root is RelativeModuleName) {
-                var imports = ModuleResolution.CurrentPathResolver.FindImports(Module.FilePath, node);
-                if (HandleImportSearchResult(imports, null, null, node.Root, out var variableModule)) {
-                    AssignVariables(node, imports, variableModule);
-                }
-            } else {
-                FindModuleByAbsoluteName(node.Root, null, node.ForceAbsolute, out _, out var module, out _, out var imports);
-                AssignVariables(node, imports, module);
-            }
-
+            FindModule(node.Root, null, node.ForceAbsolute, out var firstModule, out var lastModule, out _, out var imports);
+            AssignVariables(node, imports, lastModule);
             return false;
         }
 
