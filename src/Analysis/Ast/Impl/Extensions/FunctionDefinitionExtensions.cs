65d411010d037ec48f5d0e8ff6ac09c6fdebcc43
Cameron Trando <CTrando@users.noreply.github.com>
2019-08-15 12:18:49 -0700

    Adding extension method for determining if function is __new__ or __init__

---
+++ src/Analysis/Ast/Impl/Extensions/FunctionDefinitionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Python.Analysis.Types;
+
+namespace Microsoft.Python.Analysis.Extensions {
+    public static class ClassMemberExtensions {
+        public static bool IsDunderInit(this IPythonClassMember member) {
+            return member.Name == "__init__" && member.DeclaringType?.MemberType == PythonMemberType.Class;
+        }
+
+        public static bool IsDunderNew(this IPythonClassMember member) {
+            return member.Name == "__new__" && member.DeclaringType?.MemberType == PythonMemberType.Class;
+        }
+    }
+}
