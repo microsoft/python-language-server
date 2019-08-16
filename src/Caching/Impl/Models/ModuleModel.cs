2f299cd60d45254eebb0ea026a88adb8eb726b81
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-08-16 13:26:47 -0700

    Merge issues

--- src/Caching/Impl/Models/ModuleModel.cs
+++ src/Caching/Impl/Models/ModuleModel.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Python.Analysis.Caching.Factories;
 using Microsoft.Python.Analysis.Specializations.Typing;
 using Microsoft.Python.Analysis.Types;
 using Microsoft.Python.Analysis.Values;
