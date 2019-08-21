4bd56ff449d211a89605e64408b5058d20062404
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-08-20 17:03:38 -0700

    Using

--- src/Analysis/Core/Impl/Interpreter/ModulePath.cs
+++ src/Analysis/Core/Impl/Interpreter/ModulePath.cs
@@ -15,11 +15,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Python.Core;
 using Microsoft.Python.Core.IO;
