d74c57ef3f6f5c902f94e9e7cd301df0515b26f7
HeeJae Chang <hechang@microsoft.com>
2019-09-24 16:55:07 -0700

    use LinkedList instead of List
    simple clean up. PriorityProducerConsumer was using List but removed element from head. but never took advantage of indexer so changed it to use LinkedList.

--- src/Core/Impl/Extensions/TaskCompletionSourceExtensions.cs
+++ src/Core/Impl/Extensions/TaskCompletionSourceExtensions.cs
@@ -18,8 +18,10 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Python.Core {
     public static class TaskCompletionSourceExtensions {
+        private const int NO_DELAY = -1;
+
         public static CancellationTokenRegistration RegisterForCancellation<T>(this TaskCompletionSource<T> taskCompletionSource, CancellationToken cancellationToken) 
-            => taskCompletionSource.RegisterForCancellation(-1, cancellationToken);
+            => taskCompletionSource.RegisterForCancellation(millisecondsDelay: NO_DELAY, cancellationToken);
 
         public static CancellationTokenRegistration RegisterForCancellation<T>(this TaskCompletionSource<T> taskCompletionSource, int millisecondsDelay, CancellationToken cancellationToken) {
             if (millisecondsDelay >= 0) {
