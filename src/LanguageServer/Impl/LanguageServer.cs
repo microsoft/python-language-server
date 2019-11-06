82685d726f65c225caf144187367431e6764c511
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2019-11-05 16:05:57 -0800

    Use new using syntax

--- src/LanguageServer/Impl/LanguageServer.cs
+++ src/LanguageServer/Impl/LanguageServer.cs
@@ -217,12 +217,11 @@ namespace Microsoft.Python.LanguageServer.Implementation {
 
         [JsonRpcMethod("textDocument/codeAction")]
         public async Task<CodeAction[]> CodeAction(JToken token, CancellationToken cancellationToken) {
-            using (var timer = _requestTimer.Time("textDocument/codeAction")) {
-                await _prioritizer.DefaultPriorityAsync(cancellationToken);
-                var actions = await _server.CodeAction(ToObject<CodeActionParams>(token), cancellationToken);
-                timer.AddMeasure("count", actions?.Length ?? 0);
-                return actions;
-            }
+            using var timer = _requestTimer.Time("textDocument/codeAction");
+            await _prioritizer.DefaultPriorityAsync(cancellationToken);
+            var actions = await _server.CodeAction(ToObject<CodeActionParams>(token), cancellationToken);
+            timer.AddMeasure("count", actions?.Length ?? 0);
+            return actions;
         }
 
         //[JsonRpcMethod("textDocument/codeLens")]
