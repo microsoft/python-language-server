77c5978b4d0ea82799eae930e358834d9c584f8f
HeeJae Chang <hechang@microsoft.com>
2019-11-08 16:22:28 -0800

    adding code action setting tests

--- src/LanguageServer/Impl/CodeActions/CodeActionSettings.cs
+++ src/LanguageServer/Impl/CodeActions/CodeActionSettings.cs
@@ -13,7 +13,10 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
+using System;
 using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Python.LanguageServer.CodeActions {
     public sealed class CodeActionSettings {
@@ -21,6 +24,21 @@ namespace Microsoft.Python.LanguageServer.CodeActions {
 
         private readonly IReadOnlyDictionary<string, object> _refactoring;
         private readonly IReadOnlyDictionary<string, object> _quickFix;
+
+        public static CodeActionSettings Parse(JToken refactoring, JToken quickFix, CancellationToken cancellationToken) {
+            var refactoringMap = new Dictionary<string, object>();
+            var quickFixMap = new Dictionary<string, object>();
+
+            AppendToMap(refactoring, GetPrefixLength(refactoring?.Path), refactoringMap, cancellationToken);
+            AppendToMap(quickFix, GetPrefixLength(quickFix?.Path), quickFixMap, cancellationToken);
+
+            return new CodeActionSettings(refactoringMap, quickFixMap);
+
+            static int GetPrefixLength(string path) {
+                // +1 is for last "." after prefix
+                return string.IsNullOrEmpty(path) ? 0 : path.Length + 1;
+            }
+        }
 
         public CodeActionSettings(IReadOnlyDictionary<string, object> refactoring, IReadOnlyDictionary<string, object> quickFix) {
             _refactoring = refactoring ?? new Dictionary<string, object>();
@@ -31,21 +49,89 @@ namespace Microsoft.Python.LanguageServer.CodeActions {
         public T GetQuickFixOption<T>(string key, T defaultValue) => GetOption(_quickFix, key, defaultValue);
 
         private T GetOption<T>(IReadOnlyDictionary<string, object> map, string key, T defaultValue) {
-            if (key == null) {
-                // invalid key
-                return defaultValue;
-            }
+            try {
+                if (map.TryGetValue(key, out var value)) {
 
-            if (map.TryGetValue(key, out var value)) {
-                if (value == null) {
-                    // if value is explicitly set to null, then return null
+                    // handle boxing case properly. 
+                    // if value is boxing of "long", and asked for "int", asking for "int" on value
+                    // will fail. 
+                    // ex) value is T or (T)value will fail due to boxing.
+                    // we first need to change boxed value to boxing of T and then cast.
+                    // 
+                    // ChangeType can throw if given typeof(T) is not supported
+                    if (value is IConvertible) {
+                        value = Convert.ChangeType(value, typeof(T));
+                    }
+
                     return (T)value;
                 }
-
-                return (value is T typedValue) ? typedValue : defaultValue;
+            } catch {
+                // ignore any option failure
             }
 
             return defaultValue;
+        }
+
+        private static void AppendToMap(JToken setting, int prefixLength, Dictionary<string, object> map, CancellationToken cancellationToken) {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (setting == null || !setting.HasValues) {
+                return;
+            }
+
+            foreach (var child in setting) {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (child is JValue value) {
+                    // there shouldn't be duplicates and prefix must exist.
+                    var path = child.Path;
+                    if (path.Length <= prefixLength) {
+                        // nothing to add
+                        continue;
+                    }
+
+                    // get rid of common "settings.python..." prefix
+                    map[path.Substring(prefixLength)] = value.Value;
+                    continue;
+                }
+
+                if (child.Type == JTokenType.Array) {
+                    // support options in the form of python.refactoring.xxx = [ .... ];
+                    // in that case, key will be python.refactoring.xxx and value will be a map
+                    var nestedMap = new Dictionary<string, object>();
+                    AppendToNestedMap(child, name: null, nestedMap, cancellationToken);
+
+                    map[child.Path.Substring(prefixLength)] = nestedMap;
+                    continue;
+                }
+
+                AppendToMap(child, prefixLength, map, cancellationToken);
+            }
+
+            static void AppendToNestedMap(JToken token, string name, Dictionary<string, object> map, CancellationToken cancellationToken) {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (token == null || !token.HasValues) {
+                    return;
+                }
+
+                if (token is JProperty property) {
+                    name = name == null ? property.Name : $"{name}.{property.Name}";
+                }
+
+                foreach (var child in token) {
+                    cancellationToken.ThrowIfCancellationRequested();
+
+                    if (child is JValue value) {
+                        // there shouldn't be duplicates and prefix must exist.
+                        // get rid of common "settings.python..." prefix
+                        map[name] = value.Value;
+                        continue;
+                    }
+
+                    AppendToNestedMap(child, name, map, cancellationToken);
+                }
+            }
         }
     }
 }
---
+++ src/LanguageServer/Test/SettingTests.cs
@@ -0,0 +1,145 @@
+﻿// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Python.Analysis;
+using Microsoft.Python.Analysis.Analyzer;
+using Microsoft.Python.Core.Idle;
+using Microsoft.Python.Core.IO;
+using Microsoft.Python.Core.Services;
+using Microsoft.Python.Core.Text;
+using Microsoft.Python.LanguageServer.CodeActions;
+using Microsoft.Python.LanguageServer.Diagnostics;
+using Microsoft.Python.LanguageServer.Indexing;
+using Microsoft.Python.LanguageServer.Protocol;
+using Microsoft.Python.LanguageServer.Sources;
+using Microsoft.Python.LanguageServer.Tests.FluentAssertions;
+using Microsoft.Python.Parsing.Ast;
+using Microsoft.Python.Parsing.Tests;
+using Microsoft.Python.UnitTests.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using TestUtilities;
+
+namespace Microsoft.Python.LanguageServer.Tests {
+    [TestClass]
+    public class SettingTests : LanguageServerTestBase {
+        public TestContext TestContext { get; set; }
+        public CancellationToken CancellationToken => TestContext.CancellationTokenSource.Token;
+
+        [TestInitialize]
+        public void TestInitialize()
+            => TestEnvironmentImpl.TestInitialize($"{TestContext.FullyQualifiedTestClassName}.{TestContext.TestName}");
+
+        [TestCleanup]
+        public void Cleanup() => TestEnvironmentImpl.TestCleanup();
+
+        [TestMethod, Priority(0), Timeout(AnalysisTimeoutInMS)]
+        public void Basic() {
+            var refactoring = JToken.Parse("{ a: 1}");
+            var codeActionSetting = CodeActionSettings.Parse(refactoring, quickFix: null, CancellationToken);
+
+            codeActionSetting.GetRefactoringOption("a", -1).Should().Be(1);
+        }
+
+        private async Task TestCodeActionAsync(string markup, string title, string newText, bool enableIndexManager = false) {
+            var (analysis, codeActions, insertionSpan) =
+                await GetAnalysisAndCodeActionsAndSpanAsync(markup, MissingImportCodeActionProvider.Instance.FixableDiagnostics, enableIndexManager);
+
+            var codeAction = codeActions.Single(c => c.title == title);
+            TestCodeAction(analysis.Document.Uri, codeAction, title, insertionSpan, newText);
+        }
+
+        private async Task<(IDocumentAnalysis analysis, CodeAction[] diagnostics, SourceSpan insertionSpan)> GetAnalysisAndCodeActionsAndSpanAsync(
+            string markup, IEnumerable<string> codes, bool enableIndexManager = false) {
+            MarkupUtils.GetNamedSpans(markup, out var code, out var spans);
+
+            var analysis = await GetAnalysisAsync(code);
+
+            if (enableIndexManager) {
+                var serviceManager = (IServiceManager)analysis.ExpressionEvaluator.Services;
+                var indexManager = new IndexManager(
+                    serviceManager.GetService<IFileSystem>(),
+                    analysis.Document.Interpreter.LanguageVersion,
+                    rootPath: null,
+                    Array.Empty<string>(),
+                    Array.Empty<string>(),
+                    serviceManager.GetService<IIdleTimeService>());
+
+                // make sure index is done
+                await indexManager.IndexWorkspace(analysis.Document.Interpreter.ModuleResolution.CurrentPathResolver);
+
+                serviceManager.AddService(indexManager);
+            }
+
+            var insertionSpan = spans["insertionSpan"].First().ToSourceSpan(analysis.Ast);
+
+            var diagnostics = GetDiagnostics(analysis, spans["diagnostic"].First().ToSourceSpan(analysis.Ast), codes);
+            var codeActions = await new QuickFixCodeActionSource(analysis.ExpressionEvaluator.Services).GetCodeActionsAsync(analysis, CodeActionSettings.Default, diagnostics, CancellationToken);
+            return (analysis, codeActions.ToArray(), insertionSpan);
+        }
+
+        private static void TestCodeAction(Uri uri, CodeAction codeAction, string title, Core.Text.Range insertedSpan, string newText) {
+            codeAction.title.Should().Be(title);
+            codeAction.edit.changes.Should().HaveCount(1);
+
+            var edit = codeAction.edit.changes[uri];
+            edit.Single().range.Should().Be(insertedSpan);
+            edit.Single().newText.Should().Be(newText);
+        }
+
+        private static Diagnostic[] GetDiagnostics(IDocumentAnalysis analysis, SourceSpan span, IEnumerable<string> codes) {
+            var analyzer = analysis.ExpressionEvaluator.Services.GetService<IPythonAnalyzer>();
+            return analyzer.LintModule(analysis.Document)
+                           .Where(d => d.SourceSpan == span && codes.Any(e => string.Equals(e, d.ErrorCode)))
+                           .Select(d => d.ToDiagnostic())
+                           .ToArray();
+        }
+
+        private async Task TestCodeActionAsync(string markup, string title, string newText, string abbreviation, params string[] relativePaths) {
+            MarkupUtils.GetNamedSpans(markup, out var code, out var spans);
+
+            // get main analysis and add mock modules
+            var analysis = await GetAnalysisAsync(code);
+
+            foreach (var relativePath in relativePaths) {
+                await GetAnalysisAsync("", analysis.ExpressionEvaluator.Services, modulePath: TestData.GetTestSpecificPath(relativePath));
+            }
+
+            // calculate actions
+            var diagnosticSpan = spans["diagnostic"].First().ToSourceSpan(analysis.Ast);
+            var diagnostics = GetDiagnostics(analysis, diagnosticSpan, MissingImportCodeActionProvider.Instance.FixableDiagnostics);
+            var codeActions = await new QuickFixCodeActionSource(analysis.ExpressionEvaluator.Services).GetCodeActionsAsync(analysis, CodeActionSettings.Default, diagnostics, CancellationToken);
+
+            // verify results
+            var codeAction = codeActions.Single(c => c.title == title);
+            codeAction.edit.changes.Should().HaveCount(1);
+
+            var edits = codeAction.edit.changes[analysis.Document.Uri];
+            edits.Should().HaveCount(2);
+
+            var invocationEdit = edits.Single(e => e.newText == abbreviation);
+            invocationEdit.range.Should().Be(diagnosticSpan);
+
+            var insertEdit = edits.Single(e => e.newText == newText);
+            insertEdit.range.Should().Be(spans["insertionSpan"].First().ToSourceSpan(analysis.Ast));
+        }
+    }
+}
