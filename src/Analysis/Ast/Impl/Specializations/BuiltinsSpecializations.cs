0a55a3a2a93deea45a17f70cb398f0a63de8fba4
Mikhail Arkhipov <mikhaila@microsoft.com>
2019-11-13 08:59:36 -0800

    Usings

--- src/Analysis/Ast/Impl/Specializations/BuiltinsSpecializations.cs
+++ src/Analysis/Ast/Impl/Specializations/BuiltinsSpecializations.cs
@@ -13,7 +13,6 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
-using System.Data;
 using System.Linq;
 using Microsoft.Python.Analysis.Specializations.Typing.Types;
 using Microsoft.Python.Analysis.Specializations.Typing.Values;
