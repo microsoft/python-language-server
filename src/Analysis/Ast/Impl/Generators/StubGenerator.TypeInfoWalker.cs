9777922cf5c10a7ef8ddfa7cf215a27d93b61154
HeeJae Chang <hechang@microsoft.com>
2019-11-19 12:18:51 -0800

    add very basic type info

--- src/Analysis/Ast/Impl/Generators/StubGenerator.TypeInfoWalker.cs
+++ src/Analysis/Ast/Impl/Generators/StubGenerator.TypeInfoWalker.cs
@@ -13,32 +13,104 @@
 // See the Apache Version 2.0 License for specific language governing
 // permissions and limitations under the License.
 
-using Microsoft.Python.Analysis.Analyzer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Microsoft.Python.Analysis.Types;
+using Microsoft.Python.Analysis.Values;
 using Microsoft.Python.Core.Logging;
 using Microsoft.Python.Parsing.Ast;
 
 namespace Microsoft.Python.Analysis.Generators {
     public sealed partial class StubGenerator {
         private sealed class TypeInfoWalker : BaseWalker {
-            private readonly IExpressionEvaluator _eval;
+            private readonly Stack<IPythonType> _stack;
 
-            public TypeInfoWalker(ILogger logger, IPythonModule module, PythonAst ast, string original)
-                : base(logger, module, ast, original) {
-                _eval = Module.Analysis.ExpressionEvaluator;
+            public TypeInfoWalker(ILogger logger, IPythonModule module, PythonAst ast, string original, CancellationToken cancellationToken)
+                : base(logger, module, ast, original, cancellationToken) {
+                _stack = new Stack<IPythonType>();
             }
 
             public override bool Walk(ClassDefinition node, Node parent) {
-                return base.Walk(node, parent);
+                // no type variables for now
+                var member = GetMember(node);
+                _stack.Push(member);
+
+                return member != null;
+            }
+
+            public override void PostWalk(ClassDefinition node, Node parent) {
+                _stack.Pop();
             }
 
             public override bool Walk(FunctionDefinition node, Node parent) {
-                var member = _eval.LookupNameInScopes(node.Name, LookupOptions.All);
-                if (member != null) {
-                    // see whether we can improve generated code with our data from analysis
+                // for now, just do poorman's type info
+                var member = GetMember(node) as IPythonFunctionType;
+                if (member == null) {
+                    // no matching member
+                    return false;
                 }
 
-                return base.Walk(node, parent);
+                if (member.Overloads.Count == 1) {
+                    // if there is only 1, we just assume it is same fuction
+                    AppendMethod(node, member.Overloads[0]);
+                    return false;
+                }
+
+                var overload = member.Overloads.FirstOrDefault(m => m.Parameters.Count == node.Parameters.Length);
+                if (overload != null) {
+                    AppendMethod(node, overload);
+                }
+
+                return false;
+            }
+
+            private IPythonType GetMember(ScopeStatement node) {
+                var parent = _stack.Count > 0 ? _stack.Peek() : null;
+                if (parent != null) {
+                    return parent.GetMember(node.Name) as IPythonType;
+                }
+
+                return Module.GetMember(node.Name) as IPythonType;
+            }
+
+            private void AppendMethod(FunctionDefinition node, IPythonFunctionOverload method) {
+                var member = method.ClassMember as IPythonFunctionType;
+                var skip = member.IsStatic ? 0 : 1;
+
+                foreach (var parameter in node.Parameters.Skip(skip)) {
+                    var info = method.Parameters.FirstOrDefault(p => p.Name == parameter.Name);
+
+                    if (parameter.Annotation == null && info?.Type != null) {
+                        // for now, don't resolve type name. we could do poorman's 
+                        // create minimally qualified type name or a way to get string representation
+                        // of type at current context using imports. but for now, nothing
+                        // since I don't have ast for library, I can't use original code
+                        var nameSpan = parameter.NameExpression?.IndexSpan ?? parameter.IndexSpan;
+                        AppendOriginalText(nameSpan.End);
+                        AppendText($" : {info.Type.Name}", nameSpan.End);
+                    }
+
+                    if (parameter.DefaultValue == null && info?.DefaultValueString != null) {
+                        // for now, don't resolve type name. we could do poorman's 
+                        // create minimally qualified type name or a way to get string representation
+                        // of type at current context using imports. but for now, nothing
+                        // since I don't have ast for library, I can't use original code
+                        AppendOriginalText(parameter.IndexSpan.End);
+
+                        AppendText($" = {info.DefaultValueString}", parameter.IndexSpan.End);
+                    }
+                }
+
+                var constant = method.StaticReturnValue as IPythonConstant;
+                if (node.ReturnAnnotation == null && constant != null) {
+                    // for now, don't resolve type name. we could do poorman's 
+                    // create minimally qualified type name or a way to get string representation
+                    // of type at current context using imports. but for now, nothing
+                    // since I don't have ast for library, I can't use original code
+                    AppendOriginalText(node.HeaderIndex);
+                    AppendText($" -> {constant.Type.Name}", node.HeaderIndex);
+                }
             }
         }
     }
