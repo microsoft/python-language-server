d899165d72a612ec4aac1a9bf1ce517f5f5e582d
Mikhail Arkhipov <mikhaila@microsoft.com>
2020-10-05 22:03:26 -0700

    Declare 3.9

--- src/Parsing/Impl/PythonLanguageVersion.cs
+++ src/Parsing/Impl/PythonLanguageVersion.cs
@@ -37,7 +37,8 @@ namespace Microsoft.Python.Parsing {
         V35 = 0x0305,
         V36 = 0x0306,
         V37 = 0x0307,
-        V38 = 0x0308
+        V38 = 0x0308,
+        V39 = 0x0309
     }
 
     public static class PythonLanguageVersionExtensions {
