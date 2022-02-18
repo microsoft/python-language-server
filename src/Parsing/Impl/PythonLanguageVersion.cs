0ec927110ee4d81909ac1057a7bda163b33d2615
Adam Yoblick <Adam.Yoblick@microsoft.com>
2022-02-18 15:45:48 -0600

    Add more members to PythonLanguageVersion enum

--- src/Parsing/Impl/PythonLanguageVersion.cs
+++ src/Parsing/Impl/PythonLanguageVersion.cs
@@ -38,7 +38,9 @@ namespace Microsoft.Python.Parsing {
         V36 = 0x0306,
         V37 = 0x0307,
         V38 = 0x0308,
-        V39 = 0x0309
+        V39 = 0x0309,
+        V310 = 0x030a,
+        V311 = 0x030b,
     }
 
     public static class PythonLanguageVersionExtensions {
